93db55aa68189b4f38bf08a041e68e864a588c48
SantiValdezUlzurrun <51174811+SantiValdezUlzurrun@users.noreply.github.com>
2019-05-29 22:34:13 -0300

    Update algoturtle.py

--- algoturtle.py
+++ algoturtle.py
@@ -1,4 +1,5 @@
 from math import *
+
 class _Nodo:
 
     def __init__(self, dato=None, prox=None):
@@ -43,23 +44,29 @@ class Cola:
         if self.ultimo == None:
             return True
         return False
+
 class Pila:
+
     def __init__(self):
         self.ult = None
+
     def apilar (self,dato):
         if self.ult == None:
             self.ult = _Nodo(dato)
         else:
             dato_a_agregar = _Nodo(dato,self.ult)
             self.ult = dato_a_agregar
+
     def desapilar(self):
         dato = self.ult.dato
         self.ult = self.ult.prox
         return dato
+
     def esta_vacia(self):
         if self.ult == None:
             return True
         return False
+
     def ver_tope(self):
         return self.ult.dato
 
@@ -72,40 +79,52 @@ class Tortuga:
     orientar_arriba (suma 90 grados a orientacion)
     girar (gira una cantidad pasada por parametro)
     '''
+
     def __init__(self,orientacion = 0, posicion = [0,0],pluma = True,color ='black',ancho = 1):
         self.pluma = pluma
         self.posicion = posicion
         self.posicion_inicial = posicion
         self.orientacion = orientacion
         self.orientacion_inicial = orientacion
-        self.color = color 
+        self.color = color
         self.ancho = ancho
+
     def avanzar_x(self):
         self.posicion[0] += 10
+
     def avanzar_y(self):
         self.posicion[1] += 10
+
     def orientar_costado(self):
         self.orientacion += 180
+
     def girar_izquierda(self, cantidad):
         self.orientacion += cantidad
+
     def girar_derecha(self, cantidad):
         self.orientacion -= cantidad
+
     def avanzar(self,cantidad = [100,100]):
         self.posicion[0] += cantidad[0]
         self.posicion[1] += cantidad[1]
+
     def circulo_svg(self):
        return '<circle cx="15" cy="10" r="8" fill="white" />'
+
     def pasar_linea_svg(self):
         a = self.posicion_inicial[0]* cos(self.orientacion_inicial)
         b = self.posicion_inicial[1] * sin(self.orientacion_inicial)
         c = self.posicion[0] * cos(self.orientacion)
         d = self.posicion[1] * sin(self.orientacion)
         return f''' <line x1="{a}" y1="{b}" x2="{c}" y2="{d}" stroke-width="{self.ancho}" stroke="{self.color}" />'''
+
     def __repr__(self):
         return f'[{self.pluma},{self.posicion},{self.orientacion}]'
+
     def mover_pluma(self):
         if self.pluma:
             self.pluma = False
         else:
             self.pluma = True
-        self.avanzar()             
+        self.avanzar()
+      
