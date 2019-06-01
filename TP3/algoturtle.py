44b6f9159eff58c33f70b7ca41e9cc1e8c92f7cc
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2019-06-01 16:03:10 -0300

    Update algoturtle.py

--- TP3/algoturtle.py
+++ TP3/algoturtle.py
@@ -1,110 +1,4 @@
-#import math
-#class _Nodo:
-#
-#    def __init__(self, dato=None, prox=None):
-#        self.dato = dato
-#        self.prox = prox
-#class Cola:
-#
-#    def __init__(self):
-#        self.prim = None
-#        self.ultimo = None
-#
-#    def __len__(self):
-#        actual = self.prim
-#        contador = 0
-#        while actual:
-#            contador += 1
-#            actual = actual.prox
-#        return contador
-#
-#    def ver_primero(self):
-#        return self.prim
-#
-#    def encolar(self, dato):
-#        nuevo_nodo = _Nodo(dato)
-#        if self.ultimo is not None:
-#            self.ultimo.prox = nuevo_nodo
-#            self.ultimo = nuevo_nodo
-#        else:
-#            self.prim = nuevo_nodo
-#            self.ultimo = nuevo_nodo
-#
-#    def desencolar(self):
-#        if len(self) == 0:
-#            raise IndexError
-#        dato = self.prim.dato
-#        self.prim = self.prim.prox
-#        if not self.prim:
-#            self.ultimo = None
-#        return dato
-#
-#    def esta_vacia(self):
-#        if self.ultimo == None:
-#            return True
-#        return False
-#class Pila:
-#    def __init__(self):
-#        self.ult = None
-#    def apilar (self,dato):
-#        if self.ult == None:
-#            self.ult = _Nodo(dato)
-#        else:
-#            dato_a_agregar = _Nodo(dato,self.ult)
-#            self.ult = dato_a_agregar
-#    def desapilar(self):
-#        dato = self.ult.dato
-#        self.ult = self.ult.prox
-#        return dato
-#    def esta_vacia(self):
-#        if self.ult == None:
-#            return True
-#        return False
-#    def ver_tope(self):
-#        return self.ult.dato
-#
-#
-#class Tortuga:
-#    '''clase tortuga la cual tiene como atributos posicion ( lista de 2 numeros), orientacion (entero), pluma(true si esta abajo y false en caso contrario)
-#    como metodos:
-#    avanzar_x (avanza 10 unidades en la 1ra componente)
-#    avanzar_y (avanza 10 unidades en la 2da componente)
-#    orientar_arriba (suma 90 grados a orientacion)
-#    girar (gira una cantidad pasada por parametro)
-#    '''
-#    def __init__(self,orientacion = 0, posicion = [0,0],pluma = True,posicion_inicial = [0,0],color ='black',ancho = 1):
-#        self.pluma = pluma
-#        self.posicion = posicion
-#        self.posicion_inicial = posicion_inicial
-#        self.orientacion = orientacion
-#        self.color = color 
-#        self.ancho = ancho
-#    def orientar_costado(self):
-#        self.orientacion -= math.radians(180)
-#    def girar_izquierda(self, cantidad):
-#        self.orientacion -= math.radians(cantidad)
-#    def girar_derecha(self, cantidad):
-#        self.orientacion += math.radians(cantidad)
-#    def avanzar(self,cantidad = [10,10]):
-#        self.posicion[0] -= cantidad[0] * math.cos(self.orientacion)
-#        self.posicion[1] -= cantidad[1] * math.sin(self.orientacion)
-#    def circulo_svg(self):
-#       return '<circle cx="15" cy="10" r="8" fill="white" />'
-#    def pasar_linea_svg(self):
-#        a = self.posicion_inicial[:] 
-#        c = self.posicion[:]
-#        return f' <line x1="{a[0]}" y1="{a[1]}" x2="{c[0]}" y2="{c[1]}" stroke-width="{self.ancho}" stroke="{self.color}" />'
-#    def __repr__(self):
-#        return f'[{self.pluma},{self.posicion},{self.orientacion}]'
-#    def mover_pluma(self):
-#        if self.pluma:
-#            self.pluma = False
-#        else:
-#            self.pluma = True
-#        self.avanzar()
-#        
-#        
-from math import *
+ from math import *
 
 class _Nodo:
 
@@ -180,7 +74,7 @@ class Tortuga:
     orientar_derecha/izquierda (suma/resta x grados a orientacion)
     pasar_linea_svg (escribe en lenguaje svg lo que hizo la tortuga)
     '''
-    def __init__(self,orientacion = 0, posicion = [0,0],pluma = True,posicion_inicial = [0,0],color ='black',ancho = 1):
+    def __init__(self,orientacion = radians(90), posicion = [0,0],pluma = True,posicion_inicial = [0,0],color ='black',ancho = 1):
         self.pluma = pluma
         self.posicion = posicion
         self.posicion_inicial = posicion_inicial
@@ -204,8 +98,8 @@ class Tortuga:
             self.orientacion -= radians(360)
 
     def avanzar(self,cantidad = [10,10]):
-        self.posicion[0] += cantidad[0] * cos(self.orientacion)
-        self.posicion[1] += cantidad[1] * sin(self.orientacion)
+        self.posicion[0] -= cantidad[0] * cos(self.orientacion)
+        self.posicion[1] -= cantidad[1] * sin(self.orientacion)
 
     def circulo_svg(self):
        return '<circle cx="15" cy="10" r="8" fill="white" />'
